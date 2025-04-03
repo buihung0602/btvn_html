5b78d23a379cfe062ce6939ee5f4468a2f117aab
buihung0602 <n24dtcn035@student.ptithcm.edu.vn>
2025-04-03 11:56:17 +0700

    Add files via upload

---
+++ ss26 (1)/bài 5/index.js
@@ -0,0 +1,22 @@
+function maxNumber (array) {
+    if (!Array.isArray(array) || !array.every(item => typeof item === "number")){
+        console.log("Dữ liệu không hợp lệ");
+        return;
+    }else
+    if (array.length === 0) {
+        console.log("Mảng không có dữ liệu")
+        return;
+    }else {
+        console.log("max = " + max + "postion: " + index)
+    }
+    let max = Math.max(...aray)
+    let index = array.indexOf(max);
+}
+
+let array1 = [10, 9, 5, 11, 24, 5];
+let array2 = [ ];
+let array3 = ["abc"]
+
+maxNumber(array1);
+maxNumber(array2);
+maxNumber(array3);
